f853c1c975c8bba2b2ba44dcbb8a8d086e3a5f68
darya-startsava <daryastartsava@gmail.com>
2021-10-19 03:18:49 +0200

    feat: add widget clock

--- momentum/src/js/clock.js
+++ momentum/src/js/clock.js
@@ -1,1 +1,20 @@
-console.log('I\'m the clock!');
+const time = document.querySelector('.time');
+const day = document.querySelector('.date');
+
+function showTime() {
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString('en-US', { hour12: false });
+    time.textContent = currentTime;
+    showDate();
+    setTimeout(showTime, 1000);
+};
+
+function showDate() {
+    const date = new Date();
+    const options = { weekday: 'long', month: 'long', day: 'numeric'};
+    const currentDate = date.toLocaleDateString('en-US', options);
+    day.textContent = currentDate;
+    setTimeout(showDate, 1000);
+};
+
+showTime();
