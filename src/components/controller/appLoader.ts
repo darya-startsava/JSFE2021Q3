b0fcc5c81031b36e2b22be67df6ffaf301c15c8d
darya-startsava <daryastartsava@gmail.com>
2021-12-13 23:39:44 +0100

    refactor: change URL for creating deploy

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '90f172be91244209ba0690f7a59afce3', // получите свой ключ https://newsapi.org/
         });
     }
