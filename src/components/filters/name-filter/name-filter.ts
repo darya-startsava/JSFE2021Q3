8d9b3a3ae434b5250c26f9f4e54116ccdf57bbb7
darya-startsava <daryastartsava@gmail.com>
2021-12-21 03:01:37 +0100

    refactor: implement correct event to addEventListener

--- src/components/filters/name-filter/name-filter.ts
+++ src/components/filters/name-filter/name-filter.ts
@@ -12,13 +12,6 @@ export default class NameFilter extends Component implements Filter {
     addListener() {
         const searchInput = document.querySelector<HTMLInputElement>('.search-input');
         searchInput.addEventListener('input', () => {
-            if (searchInput.value === '') {
-                this.text = '';
-                const filtered = this.filter(this.toyCards);
-                this.onFilter(filtered);
-            }
-        });
-        searchInput.addEventListener('keyup', () => {
             this.text = searchInput.value;
             const filtered = this.filter(this.toyCards);
             this.onFilter(filtered);
