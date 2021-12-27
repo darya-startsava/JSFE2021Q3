6afda60ebdf69f5e485afd7c8f752b4d643c536d
darya-startsava <daryastartsava@gmail.com>
2021-12-28 00:40:55 +0100

    feat: add the ability to choose a background

--- src/components/settings-background/settings-background.ts
+++ src/components/settings-background/settings-background.ts
@@ -10,6 +10,20 @@ export default class SettingsBackground extends Component {
 
     render(): HTMLElement {
         this.container.innerHTML = template(SettingsBackgroundHTML)();
+        this.addListener();
         return this.container;
+    }
+
+    addListener() {
+        const backgroundItemButtons = this.container.querySelectorAll<HTMLInputElement>('.button-background-item');
+        backgroundItemButtons.forEach((item) =>
+            item.addEventListener('click', (event: Event) => {
+                backgroundItemButtons.forEach((item) => item.classList.remove('button-background-item-active'));
+                const target = event.target as HTMLElement;
+                target.classList.add('button-background-item-active');
+                const treeSection = document.querySelector<HTMLElement>('.tree-section');
+                treeSection.style.backgroundImage = `url('../../assets/backgrounds/${item.value}.jpg')`;
+            })
+        );
     }
 }
