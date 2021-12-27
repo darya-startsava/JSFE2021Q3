261504886c732b0fceeb58eb31e14f2b629c3372
darya-startsava <daryastartsava@gmail.com>
2021-12-28 00:29:42 +0100

    feat: add the ability to choose a tree

--- src/components/settings-tree/settings-tree.ts
+++ src/components/settings-tree/settings-tree.ts
@@ -10,6 +10,20 @@ export default class SettingsTree extends Component {
 
     render(): HTMLElement {
         this.container.innerHTML = template(SettingsTreeHTML)();
+        this.addListener();
         return this.container;
+    }
+
+    addListener() {
+        const treeItemButtons = this.container.querySelectorAll<HTMLInputElement>('.button-tree-item');
+        treeItemButtons.forEach((item) =>
+            item.addEventListener('click', (event: Event) => {
+                treeItemButtons.forEach((item) => item.classList.remove('button-tree-item-active'));
+                const target = event.target as HTMLElement;
+                target.classList.add('button-tree-item-active');
+                const mainTree = document.querySelector<HTMLElement>('.main-tree');
+                mainTree.style.backgroundImage = `url('../../assets/trees/${item.value}.png')`;
+            })
+        );
     }
 }
