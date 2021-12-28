441c9c9e29d3bef2b8515e40a81ce31c1054fae1
darya-startsava <daryastartsava@gmail.com>
2021-12-28 02:52:53 +0100

    feat: add snowflake

--- src/components/settings/settings.ts
+++ src/components/settings/settings.ts
@@ -10,6 +10,38 @@ export default class Settings extends Component {
 
     render(): HTMLElement {
         this.container.innerHTML = template(SettingsHTML)();
+        this.addListener();
         return this.container;
+    }
+
+    addListener() {
+        const snowButton = this.container.querySelector('.snow-button');
+        let refreshIntervalId: NodeJS.Timer;
+        snowButton.addEventListener('click', () => {
+            snowButton.classList.toggle('settings-button-active');
+            const treeSection = document.querySelector('.tree-section');
+            if (snowButton.classList.contains('settings-button-active')) {
+                refreshIntervalId = setInterval(createSnowFlake, 50);
+            } else {
+                clearInterval(refreshIntervalId);
+                const snowFlakes = document.querySelectorAll('.snowflake');
+                snowFlakes.forEach((item) => item.remove());
+            }
+            function createSnowFlake() {
+                const treeSectionRect = treeSection.getBoundingClientRect();
+                const snowFlake = document.createElement('i');
+                snowFlake.classList.add('snowflake');
+                snowFlake.style.left = treeSectionRect.left + Math.random() * treeSectionRect.width + 'px';
+                snowFlake.style.animationDuration = Math.random() * 3 + 5 + 's'; // between 2 - 5 seconds
+                snowFlake.style.opacity = Math.random().toString();
+                snowFlake.style.backgroundSize = Math.random() * 10 + 10 + 'px';
+
+                treeSection.appendChild(snowFlake);
+
+                setTimeout(() => {
+                    snowFlake.remove();
+                }, 5000);
+            }
+        });
     }
 }
