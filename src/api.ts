c837c11cf45b9972488db64904d6bb62267f0138
darya-startsava <daryastartsava@gmail.com>
2022-01-22 04:30:27 +0100

    feat: add create car method

--- src/api.ts
+++ src/api.ts
@@ -2,11 +2,24 @@ import ServerUrl from './enums/server-url-enum';
 import JSONValue from './types/json-value-type';
 
 export default async function getCars(): Promise<[JSONValue]> {
-    const carsArray = await (await fetch(ServerUrl.garage)).json();
+    const response = await fetch(ServerUrl.garage);
+    const carsArray = await response.json();
     return carsArray;
 }
 
 export async function getCar(id: Number): Promise<JSONValue> {
     const car = await (await fetch(`${ServerUrl.garage}/${id}`)).json();
+    return car;
+}
+
+export async function createCar(newCar: object): Promise<JSONValue> {
+    const response = await fetch(ServerUrl.garage, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newCar),
+    });
+    const car = await response.json();
     return car;
 }
--- src/components/garage-buttons/garage-buttons.ts
+++ src/components/garage-buttons/garage-buttons.ts
@@ -1,6 +1,8 @@
 import GarageButtonsHTML from './garage-buttons.html';
 import './garage-buttons.scss';
 import Component from '../abstract-component';
+import { createCar } from '../../api';
+import renderGaragePage from '../../pages/garage';
 
 export default class GarageButtons extends Component {
     constructor() {
@@ -9,6 +11,19 @@ export default class GarageButtons extends Component {
 
     render(): HTMLElement {
         this.container.innerHTML = GarageButtonsHTML;
+        this.addListener();
         return this.container;
+    }
+
+    addListener(): void {
+        const createCarButton = this.container.querySelector('.create-car-button');
+        const createCarName = this.container.querySelector<HTMLInputElement>('#create-car-name');
+        const createCarColor = this.container.querySelector<HTMLInputElement>('#create-car-color');
+        createCarButton?.addEventListener('click', async () => {
+            if (createCarName && createCarColor) {
+                await createCar({ name: `${createCarName.value}`, color: `${createCarColor.value}` });
+            }
+            renderGaragePage();
+        });
     }
 }
