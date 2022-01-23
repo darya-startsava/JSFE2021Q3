58245d3eb1c4b581ac003f407c4d8923e5eae74e
darya-startsava <daryastartsava@gmail.com>
2022-01-23 17:25:57 +0100

    feat: add race button

--- src/components/garage-buttons/garage-buttons.ts
+++ src/components/garage-buttons/garage-buttons.ts
@@ -1,10 +1,11 @@
 import GarageButtonsHTML from './garage-buttons.html';
 import './garage-buttons.scss';
 import Component from '../abstract-component';
-import { createCar, updateCar } from '../../api';
+import { createCar, go, updateCar } from '../../api';
 import renderGaragePage from '../../pages/garage';
 import { generateRandomCarName, generateRandomColor } from '../../generate-random-car';
 import store from '../../store';
+import store2 from '../../store2';
 
 export default class GarageButtons extends Component {
     constructor() {
@@ -49,6 +50,14 @@ export default class GarageButtons extends Component {
                 });
                 renderGaragePage();
             }
+        });
+        const raceButton = this.container.querySelector('.button-race');
+        raceButton?.addEventListener('click', async () => {
+            const promiseArray = [];
+            for (let i = 0; i < store2.carsArray.length; i++) {
+                promiseArray.push(go(store2.carsArray[i].id));
+            }
+            await Promise.all(promiseArray);
         });
     }
 }
